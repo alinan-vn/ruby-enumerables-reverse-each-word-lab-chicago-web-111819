394c5c5f4405e80c988a224512cf54b46d8ac6f9
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 15:16:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(str_arg)
   array = str_arg.split(" ")
-  array.map{|s| s.reverse}
-  
-  #array.each { |s|     }
-  return array 
+  return array.collect{|s| s.reverse}
+   
 end 
 
 puts reverse_each_word("hello how are you")
